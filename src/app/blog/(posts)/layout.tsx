c7bfe2ab84be58c68347dda9d45fd7a95e0029de
ahujaankush <ankush@ahuja.app>
2025-06-04 12:24:29 +0200

    Add blog posts for DA

--- src/app/blog/(posts)/layout.tsx
+++ src/app/blog/(posts)/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className="py-32 px-4 sm:px-6 lg:px-20">
+        <main className="py-32 px-4 sm:px-6 lg:px-0 lg:w-[60%] mx-auto space-y-2">
             {children}
         </main>
     );
--- src/components/download/download.tsx
+++ src/components/download/download.tsx
@@ -1,5 +1,4 @@
 import { Separator } from "@/components/ui/separator";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
